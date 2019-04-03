04d0c71886175731b9021db0d75e47c187cdc17c
Svitlana Filatova and Tessa Sullivan <svitlana@>
2019-04-03 13:31:20 -0700

    User gets correct response when entering a word that starts with a consonant and then qu

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 _Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an ay. Rules:_
 1. _For words that begin with consonant sounds, all letters before the initial vowel are placed at the end of the word sequence. Then, "ay" is added. Example: "pig" = "igpay"_
 2. _When words begin with consonant clusters (multiple consonants that form one sound), the whole sound is added to the end when speaking or writing. Example: "smile" = "ilesmay"_
-3. _For words that begin with vowel sounds, one just adds "way" or "yay" to the end (or just "ay"). Example: "eat" = "eatway" or "eatay"_
+3. _For words that begin with vowel sounds, one just adds "way" to the end (or just "ay"). Example: "eat" = "eatway" or "eatay"_
 4. _An alternative convention for words beginning with vowel sounds, one removes the initial vowel(s) along with the first consonant or consonant cluster. This usually only works for words with more than one syllable. Example: "every" = "eryevay"._
 
 ## Setup/Installation Requirements
@@ -17,9 +17,9 @@ _Pig Latin takes the first consonant (or consonant cluster) of an English word, 
 * _Clone this repository_
 * _Open index.html in a web browser of your choise._
 
-## Known Bugs
+## Known Bugs / Limitations
 
-_No known bugs yet._
+* Words beginning with more than 3 consonants are not handled correctly.
 
 ## Support and contact details
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
+var consonants = 'bcdfghjklmnpqrstvwyz'.split('');
+var vowels = 'aeiou'.split("");
+
+
 function translateWord(word){
   var output;
-  var consonants = 'bcdfghjklmnpqrstvwyz'.split('');
-  var vowels = 'aeiou'.split("");
+  // var consonants = 'bcdfghjklmnpqrstvwyz'.split('');
+  // var vowels = 'aeiou'.split("");
   var splitWord = word.split("");
   var startOfWord = "";
 
@@ -11,9 +15,11 @@ function translateWord(word){
     output = splitWord.join("") + 'way';
   } else {
 
+    // if word starts with 'qu'
     if (splitWord[0] === "q" && splitWord[1] === "u") {
       startOfWord = "qu";
       endOfWord = splitWord.slice(2).join("");
+    // otherwise, split off initial consonants
     } else {
       var splitWord = splitInitialConsonants(word);
       var startOfWord = splitWord[0];
@@ -29,19 +35,29 @@ function splitInitialConsonants(word) {
   // Determine whether the first 1, 2 or 3 letters are all consonants.
   var initialConsonants = "";
   var endOfWord = "";
-  var consonants = 'bcdfghjklmnprstvwyz'.split('');
   var splitWord = word.split("");
-  if (consonants.includes(splitWord[0])) {
-    initialConsonants = splitWord[0].toString();
-    endOfWord = splitWord.slice(1).join("");
-  }
-  if (consonants.includes(splitWord[1])) {
-    initialConsonants = initialConsonants + splitWord[1].toString();
-    endOfWord = splitWord.slice(2).join("");
-  }
-  if (consonants.includes(splitWord[2])) {
-    initialConsonants = initialConsonants + splitWord[2].toString();
+
+  // Handle special case with qu as not 1st letters
+  if (splitWord[1] === "q" && splitWord[2] === "u") {
+    initialConsonants = splitWord[0].toString() + "qu";
     endOfWord = splitWord.slice(3).join("");
+  } else {
+    // Capture the first letter
+    if (consonants.includes(splitWord[0])) {
+      initialConsonants = splitWord[0].toString();
+      endOfWord = splitWord.slice(1).join("");
+    }
+
+    //capture the second letter, if it's a consonant
+    if (consonants.includes(splitWord[1])) {
+      initialConsonants = initialConsonants + splitWord[1].toString();
+      endOfWord = splitWord.slice(2).join("");
+    }
+    //capture the third letter, if it's a consonant
+    if (consonants.includes(splitWord[2])) {
+      initialConsonants = initialConsonants + splitWord[2].toString();
+      endOfWord = splitWord.slice(3).join("");
+    }
   }
     return [initialConsonants, endOfWord];
 }
